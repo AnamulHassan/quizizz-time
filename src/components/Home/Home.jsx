c90f3a0bbb2970521383e22b0415ab9ca0ec4c83
AnamulHassan <itsanamulhassan@gmail.com>
2022-10-11 05:37:15 +0600

    home component responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import QuizItem from '../QuizItem/QuizItem';
 import { QuizCategoryContext } from '../Root/Root';
+import textBG from '../../assets/images/text-bg.png';
 
 const Home = () => {
   const quizCategoryData = useContext(QuizCategoryContext);
@@ -8,10 +9,15 @@ const Home = () => {
   return (
     <section>
       <div className=" w-11/12 md:w-3/4 mx-auto text-center mt-8 mb-6">
-        <h2 className="text-4xl md:text-5xl font-bold">
+        <h2 className="  text-4xl md:text-5xl font-bold">
           It matters <br />{' '}
-          <span className="text-5xl md:text-6xl font-extrabold text-[#dd392f]">
+          <span className=" relative text-5xl md:text-6xl font-extrabold text-[#dd392f] z-10">
             how you ask
+            <img
+              className="absolute  top-2/4 left-2/4 z-[-10] translate-y-[-50%] translate-x-[-50%]"
+              src={textBG}
+              alt="text background"
+            />
           </span>
         </h2>
         <p className="text-md md:text-lg font-semibold">
